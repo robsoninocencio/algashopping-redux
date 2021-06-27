fe9530ebbf7e0c292122958056313fddaec8f439
Robson Inocêncio <robson.inocencio@gmail.com>
2021-06-27 02:46:21 -0300

    install basic redux

---
+++ src/store/Calculator/Calculator.actions.js
@@ -0,0 +1,14 @@
+// action Creators
+export function sum(a, b) {
+    return {
+        type: 'SUM',
+        payload: [a, b]
+    }
+}
+
+export function subtract(a, b) {
+    return {
+        type: 'SUBTRACT',
+        payload: [a, b]
+    }
+}
